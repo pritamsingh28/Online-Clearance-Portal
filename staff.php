2b04f4595c036623d4ac0bc9bf36d52b362862df
MD Alquama Jia <98767551+Alquamajia@users.noreply.github.com>
2023-11-09 11:10:33 +0530

    Add files via upload

---
+++ staff.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="icon" href="images/icon.ico">
+    <link rel="stylesheet" href="css1/staff.css">
+    <title>  GIET University Student Clearance Portal | Welcome </title>
+    <!--- Bootstrap files-->
+    <link rel="stylesheet" type="text/css" href="../bootstrap-4.4.1-dist/css/bootstrap.min.css">
+
+    <script src="../bootstrap-4.4.1-dist/js/bootstrap.min.js" charset="utf-8"></script>
+
+    </script>
+    <style>body{
+      background-image:url("img/registration-bg.jpg");
+      background-repeat: no-repeat;
+      background-size: cover;
+    }</style>
+    <header id="main-header">
+    <div class="container">
+        <img src="images/ktu_logo.png">
+
+        <h1 style="font-size:40px"> GIET University Clearance Portal</h1>
+    </div>
+</header>
+<body>
+<nav id="navbar">
+    <div class="container">
+        <ul>
+
+            <li><a href="designeelogin.php"><img src="images/icons/icons8_Admin_48px.png"> STAFF PORTAL </a></li>
+            <li><a href="supervisor_login.php"><img src="images/icons/icons8_Admin_48px.png"> SUPERVISOR PORTAL </a></li>
+            <li><a href="hod_login.php"><img src="images/icons/icons8_Admin_48px.png"> HOD PORTAL </a></li>
+            <li><a href="login_admin.php"><img src="images/icons/icons8_Admin_48px.png"> ADMIN PORTAL </a></li>
+        </ul>
+    </div>
+</nav>
+<div class="container" style="background-color: #">
+    <section id="showcase">
+        <div class="container">
+
+        </div>
+    </section>
+</div>
+</body>
+<style>
+
+
+body{
+    padding-top: 60px;
+    padding-bottom: 40px;
+}
+.fixed-header, .fixed-footer{
+    width: 100%;
+    position: fixed;
+    background:#04126E;
+    padding: 10px 0;
+    color: #fff;
+}
+.fixed-header{
+    top: 0;
+}
+.fixed-footer{
+    bottom: 0;
+}
+.container{
+    width: 80%;
+    margin: 0 auto; /* Center the DIV horizontally */
+}
+#navbar a{
+      color: black;
+      text-decoration: none;
+      font-size:18px;
+      padding:5px;
+  }
+#navbar a:hover{
+    background-color: #ff2fb3;
+  }
+
+
+
+
+
+
+
+</style>
+<div class="fixed-footer">
+        <div class="container">Copyright &copy; GIET University Student Clearance </div>
+    </div>
+</div>
+</html>
