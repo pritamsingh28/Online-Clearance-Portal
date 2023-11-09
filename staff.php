7796105b796f70478476df3f72a94d9322bd4e86
MD Alquama Jia <mdalquamajia@gmail.com>
2023-11-09 15:53:04 +0530

    Staff page completed

--- staff.php
+++ staff.php
@@ -4,13 +4,35 @@
     <meta charset="utf-8">
     <link rel="icon" href="images/icon.ico">
     <link rel="stylesheet" href="css1/staff.css">
+    <!-- Tell the browser to be responsive to screen width -->
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <!-- Font Awesome -->
+ <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+ <!-- Ionicons -->
+ <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+ <!-- Tempusdominus Bbootstrap 4 -->
+ <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+ <!-- iCheck -->
+ <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+ <!-- JQVMap -->
+ <link rel="stylesheet" href="plugins/jqvmap/jqvmap.min.css">
+ <!-- Theme style -->
+ <link rel="stylesheet" href="dist/css/adminlte.min.css">
+ <!-- overlayScrollbars -->
+ <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+ <!-- Daterange picker -->
+ <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
+ <!-- summernote -->
+ <link rel="stylesheet" href="plugins/summernote/summernote-bs4.css">
+ <!-- Google Font: Source Sans Pro -->
+ <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
     <title>  GIET University Student Clearance Portal | Welcome </title>
     <!--- Bootstrap files-->
     <link rel="stylesheet" type="text/css" href="../bootstrap-4.4.1-dist/css/bootstrap.min.css">
 
     <script src="../bootstrap-4.4.1-dist/js/bootstrap.min.js" charset="utf-8"></script>
 
-    </script>
+
     <style>body{
       background-image:url("img/registration-bg.jpg");
       background-repeat: no-repeat;
@@ -18,16 +40,15 @@
     }</style>
     <header id="main-header">
     <div class="container">
-        <img src="images/ktu_logo.png">
+        <img src="images/gietlogo.jpg">
 
-        <h1 style="font-size:40px"> GIET University Clearance Portal</h1>
+        <h1 style="font-size:40px"><b> GIET University Clearance Portal</h1>
     </div>
 </header>
 <body>
 <nav id="navbar">
     <div class="container">
         <ul>
-
             <li><a href="designeelogin.php"><img src="images/icons/icons8_Admin_48px.png"> STAFF PORTAL </a></li>
             <li><a href="supervisor_login.php"><img src="images/icons/icons8_Admin_48px.png"> SUPERVISOR PORTAL </a></li>
             <li><a href="hod_login.php"><img src="images/icons/icons8_Admin_48px.png"> HOD PORTAL </a></li>
@@ -87,5 +108,42 @@ body{
 <div class="fixed-footer">
         <div class="container">Copyright &copy; GIET University Student Clearance </div>
     </div>
-</div>
+    <!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="plugins/jquery-ui/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- ChartJS -->
+<script src="plugins/chart.js/Chart.min.js"></script>
+<!-- Sparkline -->
+<script src="plugins/sparklines/sparkline.js"></script>
+<!-- JQVMap -->
+<script src="plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="plugins/jqvmap/maps/jquery.vmap.world.js"></script>
+<!-- jQuery Knob Chart -->
+<script src="plugins/jquery-knob/jquery.knob.min.js"></script>
+<!-- daterangepicker -->
+<script src="plugins/moment/moment.min.js"></script>
+<script src="plugins/daterangepicker/daterangepicker.js"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<!-- Summernote -->
+<script src="plugins/summernote/summernote-bs4.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- FastClick -->
+<script src="plugins/fastclick/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="dist/js/pages/dashboard.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="dist/js/demo.js"></script>
+
 </html>
+
